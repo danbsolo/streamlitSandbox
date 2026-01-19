d68455bc15f20eec42fec954646f71de6e911511
Daniel <80925152+danbsolo@users.noreply.github.com>
2026-01-19 11:58:17 -0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+
+
+# Write directly to the app
+st.title(f"This is a title.")
+st.write(
+  """This is a subtitle.
+  """
+)
+
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+myRequest = f"https://my.smoothiefroot.com/api/fruit/watermelon"
+
+st.write(f"Request output: {requests.get(myRequest)}")

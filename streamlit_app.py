0ddfc76ea5c925154b288083b653550d9987b0cc
Daniel <80925152+danbsolo@users.noreply.github.com>
2026-01-19 12:05:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,31 @@ st.write(f"Request output: {requests.get(myRequest)}")
 privateKey = st.text_input("Key here", "placeholder")
 st.write("Your private key is", privateKey)
 
+
+if privateKey:
+  url = "https://FINCAN-POC.snowflakecomputing.com/api/v2/databases/TAX_POLICY/schemas/GST/agents/TESTAGENT:run"
+  
+  headers = {
+      "Authorization": "Bearer " + privateKey,
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+  }
+  
+  payload = {
+      "messages": [
+          {
+              "role": "user",
+              "content": [
+                  {
+                      "type": "text",
+                      "text": "What are the sales trends?"
+                  }
+              ]
+          }
+      ]
+  }
+  
+  response = requests.post(url, headers=headers, json=payload)
+  response.raise_for_status()
+  
+  # st.write(response.json())
